49c5526b7b4fcb5b34a38eed02f69cdd11139863
Mixe101 <ejbolts@hotmail.com>
2022-12-06 04:48:30 +1000

    Added scollbars for sub menu and list items, test data from api added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const songTitle = document.querySelector(".songTitle");
 const audioSong = document.querySelector("audio");
 const songImage = document.querySelector(".songImage");
 const loadingSong = document.querySelector(".loading");
-const myContent = document.querySelector("#myContent");
+const songObject = document.querySelector(".song-object");
 let songList = document.querySelector(".songList");
 const songItem = document.querySelector(".songItem");
 let test = document.querySelector(".test");
@@ -17,38 +17,38 @@ const playlist = document.querySelector(".playlist");
 const recentplayed = document.querySelector(".recently-played");
 
 playlist.addEventListener("click", () => {
-  document.querySelector(".sub-menu-list").style.display = "block";
+  document.querySelector(".sub-menu-playlist").style.display = "block";
   document.querySelector(".sub-menu-recent-songs").style.display = "none";
 });
 
 recentplayed.addEventListener("click", () => {
-  document.querySelector(".sub-menu-list").style.display = "none";
+  document.querySelector(".sub-menu-playlist").style.display = "none";
   document.querySelector(".sub-menu-recent-songs").style.display = "block";
 });
 function isAudioPLaying() {
   return musicPlayer.classList.contains("playing");
 }
 
-// async function retrieveSongs() {
-//   await fetch(
-//     "https://api.napster.com/v2.2/tracks/top?range=year&apikey=ZmMyMDNkYTktZTFhNi00NzIwLTlhZWEtMWIzZGZjZTVjNTlm"
-//   )
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Network response failed");
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       songs = data.tracks;
-//       console.log(songs);
+async function retrieveSongs() {
+  await fetch(
+    "http://api.napster.com/v2.2/artists/Art.28463069/tracks?apikey=YTkxZTRhNzAtODdlNy00ZjMzLTg0MWItOTc0NmZmNjU4Yzk4&limit=5"
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response failed");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      songs = data.tracks;
+      console.log(songs);
 
-//       loadtest(songs);
-//     })
-//     .catch((error) =>
-//       console.error("There was an issue with fetch request", error)
-//     );
-// }
+      loadtest(songs);
+    })
+    .catch((error) =>
+      console.error("There was an issue with fetch request", error)
+    );
+}
 
 function playAudio() {
   musicPlayer.classList.add("playing");
@@ -63,18 +63,37 @@ function pauseAudio() {
   audioSong.pause();
 }
 
-// function loadtest(songs) {
-//   const showInHtml = songs.map((song) => {
-//     let albumArt = song.albumId;
-//     let simg = `https://api.napster.com/imageserver/v2/albums/${albumArt}/images/200x200.jpg`;
+function loadtest(songs) {
+  const showInHtml = songs.map((song) => {
+    let albumArt = song.albumId;
 
-//     return `
-//         <img class="iamge" src=${simg}>
-//         <h5 class="card-title">${song.name}</h5>
-//         `;
-//   });
-//   myContent.innerHTML = showInHtml;
-// }
+    let simg = `https://api.napster.com/imageserver/v2/albums/${albumArt}/images/200x200.jpg`;
+
+    return `
+    
+        <div style="display: flex; text-align: left; align-items:center;  "> 
+
+        <div style="text-align: left; width:300px "> 
+        <img class="image" src=${simg} style=" width: 50%; height: auto;">
+        </div>
+        
+        <div style="text-align: left; width:300px"> 
+        <h5 class="card-title " >${song.name}</h5>
+        </div>
+        <div style="text-align: left; width:300px"> 
+        <h5 class="card-title " >${song.artistName}</h5>
+        </div>
+        <div style="text-align: left; width:300px"> 
+        <h5 class="card-title" >${song.playbackSeconds}</h5>
+        </div>
+        
+        </div>
+        `;
+  });
+  console.log(showInHtml);
+  songObject.innerHTML = showInHtml.join(" ");
+  console.log(songObject);
+}
 
 function perviousSong() {
   isAudioPLaying() ? playAudio() : pauseAudio();
@@ -100,4 +119,4 @@ playButton.addEventListener("click", () => {
   isAudioPLaying() ? pauseAudio() : playAudio(); // is audio play true? then pause else play
 });
 
-// retrieveSongs();;
+retrieveSongs();
